748d28d4a0fb39e8cf172162a4f84caa0be4c02d
julianajjo <julianajesusoliveira.jjo@gmail.com>
2021-07-05 21:47:34 -0300

    Creating views and routes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # nlw-discover
- Projeto desenvolvido durante o evento NLW da Rocketseat.
+Projeto desenvolvido durante a semana NLW Rockeseat na trilha Discover, que consiste em uma aplicação para gerenciar perguntas anônimas de usuários direcionadas ao dono da página.
---
+++ public/scripts/modal.js
@@ -0,0 +1,23 @@
+export default function Modal(){
+
+    const modalWrapper = document.querySelector('.modal-wrapper')
+
+    const cancelButton = document.querySelector('.button.cancel')
+
+    cancelButton.addEventListener("click", close)
+
+    function open(){
+        // atribuir a classe active para modal
+        modalWrapper.classList.add("active")
+    }
+    function close(){
+        // remover a classe active do modal
+        modalWrapper.classList.remove("active")
+
+    }
+
+    return{
+        open,
+        close
+    }
+}
